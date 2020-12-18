381de195ef2e5f5d3bad74af5fff5d631fa9146c
Amine L'ch <aminelch95@gmail.com>
2020-12-18 14:52:04 +0100

    install  hassankhan config package

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Noodlehaus\\' => array($vendorDir . '/hassankhan/config/src'),
     'Facebook\\' => array($vendorDir . '/facebook/php-sdk-v4/src/Facebook'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,6 +11,10 @@ class ComposerStaticInita24fc4988d5ae57cd424a4244968dd86
     );
 
     public static $prefixLengthsPsr4 = array (
+        'N' => 
+        array (
+            'Noodlehaus\\' => 11,
+        ),
         'F' => 
         array (
             'Facebook\\' => 9,
@@ -18,6 +22,10 @@ class ComposerStaticInita24fc4988d5ae57cd424a4244968dd86
     );
 
     public static $prefixDirsPsr4 = array (
+        'Noodlehaus\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hassankhan/config/src',
+        ),
         'Facebook\\' => 
         array (
             0 => __DIR__ . '/..' . '/facebook/php-sdk-v4/src/Facebook',
