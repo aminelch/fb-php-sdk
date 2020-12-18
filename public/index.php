a3b9cea3fcc6f959f9467894c1d973d96daa6323
Amine L'ch <aminelch95@gmail.com>
2020-12-18 15:37:13 +0100

    creating a helper for getting stored config

--- public/index.php
+++ public/index.php
@@ -3,12 +3,25 @@
 //load the composer autoload file
 
 use App\MaClasse;
+use Noodlehaus\Config;
+use Noodlehaus\ConfigInterface;
+use Noodlehaus\Writer\WriterInterface;
 
 require '../vendor/autoload.php';
 
 $maClasse = new MaClasse();
 
-dump($maClasse);
 
+
+$conf = Config::load("../config/app.json");
+
+// $debug = $conf->get('fb_app_id');
+
+// dump($debug);
+
+// $writer= WriterInterface();
+dump($conf->getsupportedParsers);
+
+// wlh glegt -_-
 
 echo "<h2>".$maClasse->sayHello() ." </h2>";
---
+++ src/Helper/LoadConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helper;
+
+/**
+ *
+ */
+class LoadConfig
+{
+    /**
+     * where the config is stored
+     */
+    const CONFIG_DIR = "../config/app.json";
+
+    public function __construct()
+    {
+        echo "hello";
+    }
+}
